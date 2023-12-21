41937263578f40fd8fd6083f52d823bdfa9385a1
mrudula666 <nmrchandra3@gmail.com>
2023-12-20 18:15:56 -0800

    committed StoreEntity.java class

--- src/main/java/com/kosuri/stores/dao/StoreEntity.java
+++ src/main/java/com/kosuri/stores/dao/StoreEntity.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "store_info")
 public class StoreEntity {
@@ -22,7 +26,7 @@ public class StoreEntity {
     private @Column(name = "Contact_Person_Name") String owner;
     private @Column(name = "Phone_Number") String ownerContact;
     private @Column(name = "Secondary_Phone") String secondaryContact;
-    private @Column(name = "Emailid") String ownerEmail;
+    private @Column(name = "EmailId") String ownerEmail;
     private @Column(name = "Reg_Date") String registrationDate;
     private @Column(name = "Time_Stamp") String creationTimeStamp;
     private @Column(name = "Role") String role;
@@ -31,165 +35,7 @@ public class StoreEntity {
     private @Column(name = "Modified_Date") String modifiedDate;
     private @Column(name = "Modified_Time_Stamp") String modifiedTimeStamp;
     private @Column(name = "Status") String status;
-    private @Column(name = "Password") String password;
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(String pincode) {
-        this.pincode = pincode;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public String getOwnerContact() {
-        return ownerContact;
-    }
-
-    public void setOwnerContact(String ownerContact) {
-        this.ownerContact = ownerContact;
-    }
-
-    public String getSecondaryContact() {
-        return secondaryContact;
-    }
-
-    public void setSecondaryContact(String secondaryContact) {
-        this.secondaryContact = secondaryContact;
-    }
-
-    public String getOwnerEmail() {
-        return ownerEmail;
-    }
-
-    public void setOwnerEmail(String ownerEmail) {
-        this.ownerEmail = ownerEmail;
-    }
-
-    public String getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(String registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public String getCreationTimeStamp() {
-        return creationTimeStamp;
-    }
-
-    public void setCreationTimeStamp(String creationTimeStamp) {
-        this.creationTimeStamp = creationTimeStamp;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getAddedBy() {
-        return addedBy;
-    }
-
-    public void setAddedBy(String addedBy) {
-        this.addedBy = addedBy;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public String getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(String modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public String getModifiedTimeStamp() {
-        return modifiedTimeStamp;
-    }
-
-    public void setModifiedTimeStamp(String modifiedTimeStamp) {
-        this.modifiedTimeStamp = modifiedTimeStamp;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private @Column(name = "store_verified_status") String storeVerifiedStatus;
+    private @Column(name = "expiry date") String expiryDate;
+    private @Column(name = "current_plan") String currentPlan;
 }
